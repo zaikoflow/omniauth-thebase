9fa0f665658b95a35b01ff586b66d102e1c0c48e
nagayama <nagayama@iroirodesign.com>
2016-03-26 00:52:18 +0900

    eliminate a mismatch redirect uri for omniauth-oauth2 1.4.0

--- lib/omniauth/strategies/thebase.rb
+++ lib/omniauth/strategies/thebase.rb
@@ -18,6 +18,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('/1/users/me').parsed["user"] || {}
       end
+
+      def callback_url #FIXME eliminate a mismatch redirect uri (https://github.com/intridea/omniauth-oauth2/pull/70)
+        full_host + script_name + callback_path
+      end
     end
   end
 end
